6b952cc9b7bcc39be83bae4f1ce6d20b2208f47e
Sanam-Memon <aftab.sanam@student.oaklandcc.edu>
2026-01-29 10:22:29 -0500

    created a react page

---
+++ social-media-react/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Social Media</h1>
+
+      <p>
+        Social media is a digital platform that allows people to communicate,
+        share ideas, and connect with others online. Popular platforms include
+        Instagram, TikTok, Facebook, and X.
+      </p>
+
+      <p>
+        One of the biggest advantages of social media is connection. It allows
+        people to stay in touch with friends and family, build communities, and
+        share experiences instantly.
+      </p>
+
+      <p>
+        However, social media can also have negative effects. Excessive use can
+        impact mental health, reduce productivity, and spread misinformation if
+        users are not careful.
+      </p>
+    </div>
+  )
+}
+
+export default App
+
+  
